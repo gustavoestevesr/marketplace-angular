ae54b333fd09ff43c905eafe85bbb725bdcb62e7
gustavoestevesr <gustavoesteves@directon.com.br>
2024-05-15 11:11:34 -0300

    feat: componente de alerta de promoção

---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from '../models/product.mode';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-product-alerts',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-alerts.component.html',
+  styleUrl: './product-alerts.component.scss'
+})
+export class ProductAlertsComponent {
+  @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
+}
